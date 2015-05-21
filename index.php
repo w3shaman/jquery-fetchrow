4c95e6c835bdb7bb556fe72cd8af44ef0516ab38
w3shaman <bogeyman2007@gmail.com>
2015-05-21 20:26:22 +0700

    Fix indentation.

--- index.php
+++ index.php
@@ -1,99 +1,98 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<!DOCTYPE html>
 <html>
 <head>
-	<title>JQuery FetchRow</title>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-	<script language="javascript" type="text/javascript" src="js/jquery.fetchrow.js"></script>
+  <title>JQuery FetchRow</title>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+  <script src="js/jquery.fetchrow.js"></script>
 </head>
 <body>
-	<h1>JQuery FetchRow Demo</h1>
-	<br/>
-	<form action="" method="post">
-		<p>Fetch row when <b>Enter</b> is pressed.</p>
-		<table>
-			<tr><td>ID</td><td>:</td><td><input type="text" name="item_id" id="item_id" /> * Type item ID and press Enter.</td></tr>
-			<tr><td>Item name</td><td>:</td><td><input type="text" name="item_name" id="item_name" /></td></tr>
-			<tr><td>Qty</td><td>:</td><td><input type="text" name="item_qty" id="item_qty" /></td></tr>
-		</table>
-		<br/>
-		<input type="submit" name="submit" id="submit" />
-		<script language="javascript" type="text/javascript">
-			$("#item_id").fetchrow({
-				url : "data.php?id=",
-				onPopulated : function(data, textfield){
-					$("#item_name").val(data.name);
-					$("#item_qty").val(data.qty);
-				},
-				onNullPopulated : function(textfield){
-					alert('Item not found!');
-					$("#item_name").val("");
-					$("#item_qty").val("");
-				}
-			});
-		</script>
-	</form>
-	<br/>
-	<form action="" method="post">
-		<p>Fetch row when <b>Lost focus</b>.</p>
-		<table>
-			<tr><td>ID</td><td>:</td><td><input type="text" name="item_id2" id="item_id2" /></td></tr>
-			<tr><td>Item name</td><td>:</td><td><input type="text" name="item_name2" id="item_name2" /></td></tr>
-			<tr><td>Qty</td><td>:</td><td><input type="text" name="item_qty2" id="item_qty2" /></td></tr>
-		</table>
-		<br/>
-		<input type="submit" name="submit2" id="submit2" />
-		<script language="javascript" type="text/javascript">
-			$("#item_id2").fetchrow({
-				url : "data.php?id=",
-				trigger : "blur",
-				onPopulated : function(data, textfield){
-					$("#item_name2").val(data.name);
-					$("#item_qty2").val(data.qty);
-				},
-				onNullPopulated : function(textfield){
-					alert('Item not found!');
-					$("#item_name2").val("");
-					$("#item_qty2").val("");
-				}
-			});
-		</script>
-	</form>
-	<br/>
-	<form action="" method="post">
-		<p>Fetch row when <b>On item change</b>.</p>
-		<table>
-			<tr>
-				<td>ID</td>
-				<td>:</td>
-				<td>
-					<select name="item_id3" id="item_id3">
-						<option value="">-</option>
-						<option value="1">1</option>
-						<option value="2">2</option>
-						<option value="3">3</option>
-					</select>
-				</td>
-			</tr>
-			<tr><td>Item name</td><td>:</td><td><input type="text" name="item_name3" id="item_name3" /></td></tr>
-			<tr><td>Qty</td><td>:</td><td><input type="text" name="item_qty3" id="item_qty3" /></td></tr>
-		</table>
-		<br/>
-		<input type="submit" name="submit3" id="submit3" />
-		<script language="javascript" type="text/javascript">
-			$("#item_id3").fetchrow({
-				url : "data.php?id=",
-				trigger : "change",
-				onPopulated : function(data, textfield){
-					$("#item_name3").val(data.name);
-					$("#item_qty3").val(data.qty);
-				},
-				onNullPopulated : function(textfield){
-					$("#item_name3").val("");
-					$("#item_qty3").val("");
-				}
-			});
-		</script>
-	</form>
-	<p>&nbsp;</p>
+  <h1>JQuery FetchRow Demo</h1>
+  <br/>
+  <form action="" method="post">
+    <p>Fetch row when <b>Enter</b> is pressed.</p>
+    <table>
+      <tr><td>ID</td><td>:</td><td><input type="text" name="item_id" id="item_id" /> * Type item ID and press Enter.</td></tr>
+      <tr><td>Item name</td><td>:</td><td><input type="text" name="item_name" id="item_name" /></td></tr>
+      <tr><td>Qty</td><td>:</td><td><input type="text" name="item_qty" id="item_qty" /></td></tr>
+    </table>
+    <br/>
+    <input type="submit" name="submit" id="submit" />
+    <script language="javascript" type="text/javascript">
+    $("#item_id").fetchrow({
+      url : "data.php?id=",
+      onPopulated : function(data, textfield){
+        $("#item_name").val(data.name);
+        $("#item_qty").val(data.qty);
+      },
+      onNullPopulated : function(textfield){
+        alert('Item not found!');
+        $("#item_name").val("");
+        $("#item_qty").val("");
+      }
+    });
+    </script>
+  </form>
+  <br/>
+  <form action="" method="post">
+    <p>Fetch row when <b>Lost focus</b>.</p>
+    <table>
+      <tr><td>ID</td><td>:</td><td><input type="text" name="item_id2" id="item_id2" /></td></tr>
+      <tr><td>Item name</td><td>:</td><td><input type="text" name="item_name2" id="item_name2" /></td></tr>
+      <tr><td>Qty</td><td>:</td><td><input type="text" name="item_qty2" id="item_qty2" /></td></tr>
+    </table>
+    <br/>
+    <input type="submit" name="submit2" id="submit2" />
+    <script language="javascript" type="text/javascript">
+    $("#item_id2").fetchrow({
+      url : "data.php?id=",
+      trigger : "blur",
+      onPopulated : function(data, textfield){
+        $("#item_name2").val(data.name);
+        $("#item_qty2").val(data.qty);
+      },
+      onNullPopulated : function(textfield){
+        alert('Item not found!');
+        $("#item_name2").val("");
+        $("#item_qty2").val("");
+      }
+    });
+    </script>
+  </form>
+  <br/>
+  <form action="" method="post">
+    <p>Fetch row when <b>On item change</b>.</p>
+    <table>
+      <tr>
+        <td>ID</td><td>:</td>
+        <td>
+          <select name="item_id3" id="item_id3">
+            <option value="">-</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+        </td>
+      </tr>
+      <tr><td>Item name</td><td>:</td><td><input type="text" name="item_name3" id="item_name3" /></td></tr>
+      <tr><td>Qty</td><td>:</td><td><input type="text" name="item_qty3" id="item_qty3" /></td></tr>
+    </table>
+    <br/>
+    <input type="submit" name="submit3" id="submit3" />
+    <script language="javascript" type="text/javascript">
+    $("#item_id3").fetchrow({
+      url : "data.php?id=",
+      trigger : "change",
+      onPopulated : function(data, textfield){
+        $("#item_name3").val(data.name);
+        $("#item_qty3").val(data.qty);
+      },
+      onNullPopulated : function(textfield){
+        $("#item_name3").val("");
+        $("#item_qty3").val("");
+      }
+    });
+    </script>
+  </form>
+  <p>&nbsp;</p>
 </body>
 </html>
--- js/jquery.fetchrow.js
+++ js/jquery.fetchrow.js
@@ -8,11 +8,11 @@
  */
 
 (function($) {
-	function fetchrow(settings, element){
-		var me=this;
+  function fetchrow(settings, element){
+    var me=this;
 
-		this.preventEnter=function(){
-			element.keypress(
+    this.preventEnter=function(){
+      element.keypress(
         function(e){
           if(e.keyCode==13){
             return false;
@@ -21,80 +21,80 @@
           return true;
         }
       );
-		}
-
-		this.doRequest = function() {
-			$.ajax({
-				url : settings.url + element.val(),
-				success : function(result){
-					try{
-						data = $.parseJSON(result);
-						if(typeof settings.onPopulated === "function"){
-							if(data == null){
-								if(typeof settings.onNullPopulated === "function"){
-									settings.onNullPopulated.call(this, element);
-								}
-							}
-							else{
-								settings.onPopulated.call(this, data, element);
-							}
-						}
-					}
-					catch(e){
-						alert('Sorry, an error has occured!');
-					}
-				},
-				error : function(xhr, status, ex){
-					alert('Sorry, an error has occured!');
-				}
-			});
-		}
-
-		var arr = settings.trigger.split("|");
-		switch(arr[0]) {
-			case "keypress" :
-				element.keyup(
-					function(e){
-						if(e.keyCode == arr[1]){
-							me.doRequest();
-						}
-					}
-				);
-
-				element.attr("autocomplete", "off");
-
-				this.preventEnter();
-
-				break;
-			case "blur":
-				element.blur(
-					function(e){
-						me.doRequest();
-					}
-				);
-
-				break;
-			case "change":
-				element.change(
-					function(e){
-						me.doRequest();
-					}
-				);
-
-				break;
     }
-	}
 
-	$.fn.fetchrow = function(options) {
-		var settings = {
-			onPopulated : null,
-			onNullPopulated : null,
-			trigger: 'keypress|13' // Supported events are: keypress|<keycode>, change, blur
-		};
-		$.extend(settings, options);
+    this.doRequest = function() {
+      $.ajax({
+        url : settings.url + element.val(),
+        success : function(result){
+          try{
+            data = $.parseJSON(result);
+            if(typeof settings.onPopulated === "function"){
+              if(data == null){
+                if(typeof settings.onNullPopulated === "function"){
+                  settings.onNullPopulated.call(this, element);
+                }
+              }
+              else{
+                settings.onPopulated.call(this, data, element);
+              }
+            }
+          }
+          catch(e){
+            alert('Sorry, an error has occured!');
+          }
+        },
+        error : function(xhr, status, ex){
+          alert('Sorry, an error has occured!');
+        }
+      });
+    }
 
-		return this.each(function() {
-			var obj = new fetchrow(settings, $(this));
-		});
-	}
+    var arr = settings.trigger.split("|");
+    switch(arr[0]) {
+      case "keypress" :
+        element.keyup(
+          function(e){
+            if(e.keyCode == arr[1]){
+              me.doRequest();
+            }
+          }
+        );
+
+        element.attr("autocomplete", "off");
+
+        this.preventEnter();
+
+        break;
+      case "blur":
+        element.blur(
+          function(e){
+            me.doRequest();
+          }
+        );
+
+        break;
+      case "change":
+        element.change(
+          function(e){
+            me.doRequest();
+          }
+        );
+
+        break;
+    }
+  }
+
+  $.fn.fetchrow = function(options) {
+    var settings = {
+      onPopulated : null,
+      onNullPopulated : null,
+      trigger: 'keypress|13' // Supported events are: keypress|<keycode>, change, blur
+    };
+    $.extend(settings, options);
+
+    return this.each(function() {
+      var obj = new fetchrow(settings, $(this));
+    });
+  }
 })(jQuery);
