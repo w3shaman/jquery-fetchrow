adb8680cd6d455c9cea07fcbc1d73da4d8de4617
w3shaman <bogeyman2007@gmail.com>
2017-09-03 13:24:53 +0700

    Use jQuery 3.

--- example/index.php
+++ example/index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <title>JQuery FetchRow</title>
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="../js/jquery.fetchrow.js"></script>
 </head>
 <body>
